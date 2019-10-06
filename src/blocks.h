f2380c59ee79e374db566759b61a3cbda1b704f3
Vitor Duarte <victorduart366@gmail.com>
2019-10-06 14:52:03 -0300

    its creted the function blocks_nextblock(bc)

--- src/blocks.h
+++ src/blocks.h
@@ -4,9 +4,10 @@ struct state {
     char state[SIZE][SIZE];
     int nbytes;
 };
-
+typedef struct state State;
 typedef struct node* Blockchain;
 
 Blockchain* blocks_create();
 void blocks_initialize(Blockchain* bc, char * src);
 void blocks_print(Blockchain* bc);
+State blocks_nextblock(Blockchain * bc);
