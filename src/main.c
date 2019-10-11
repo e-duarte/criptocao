26bd7222393cc0cd4a83030874d998dbd22eb86e
Ewerton Duarte <joaosouza@alunos.utfpr.edu.br>
2019-10-11 01:50:56 -0300

    Erros com o parse resolvidos

--- src/main.c
+++ src/main.c
@@ -6,9 +6,10 @@
 
 
 int main (int argc, char** argv){
-    if(argc > 0){
+    printf("%d\n", argc);
+    if(argc > 1){
         if(!strcmp(argv[1],"-c")){
-            if(argc == 2)
+            if(argc == 3)
                 cripto_encrypt(argv[2]);
             else{
                 if(argc == 1)
@@ -18,8 +19,8 @@ int main (int argc, char** argv){
             }
                 
         }else if(!strcmp(argv[1],"-d")){
-            if(argc == 3)
-                cripto_decipher(argv[2], argv[1]);
+            if(argc == 4)
+                cripto_decipher(argv[2], argv[3]);
             else{
                 if(argc == 2)
                     printf("[ERRO]FALTA O CAMINHO DO ARQUIVO\n");
