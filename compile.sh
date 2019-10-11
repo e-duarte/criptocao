b337c94d8a995e2606ec51d3dcf9a3c0a1002618
Ewerton Duarte <joaosouza@alunos.utfpr.edu.br>
2019-10-11 01:28:05 -0300

    alguma coisa

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd obj/
-gcc -c ../src/util.c
-gcc -c ../src/blocks.c
-gcc -c ../src/criptocao.c
-gcc -c ../src/main.c
-cd ../bin/
-gcc -o monstrinho ../obj/util.o ../obj/blocks.o ../obj/criptocao.o ../obj/main.o
-
